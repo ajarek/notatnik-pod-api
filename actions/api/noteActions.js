2e90a9e26812a4d665b2a29e9f6e3217dc1986bd
ajarek <ajarek@poczta.onet.pl>
2021-09-11 23:14:59 +0200

    my backend

--- actions/api/noteActions.js
+++ actions/api/noteActions.js
@@ -1,31 +1,47 @@
 const Note = require('../../db/models/note')
 
 class NoteActions {
-    saveNote(req, res) {
-        //    const newNote=new Note({
-        //        title:'Zrobić zakupy',
-        //        body:'kawa,herbata,cukier'
-        //    })
-        //    newNote.save().then(()=>{
-        //        console.log(('notatka została zapisana'));
-        //    })
+    async saveNote(req, res) {
         const title = req.body.title;
         const body = req.body.body;
-        res.send('Notatka została stworzona.Tytuł: '+title+' Treść: '+body)
-    }
-    getAllNotes(req, res) {
-        res.send('API działa')
-    }
-    getNote(req, res) {
-        res.send('Info o notatce')
-    }
-    updateNote(req, res) {
 
-        res.send('Notatka została zaktualizowana')
+        let note
+        try{
+         note = new Note({title,body})
+        await note.save()
+        }
+        catch(err){
+        return res.status(422).json({ message : err.message})
+        }
+        res.status(201).json(note)
     }
-    deleteNote(req, res) {
+    async getAllNotes(req, res) {
+        const doc = await Note.find({})
+        res.status(200).json(doc)
+    }
+
+
+    async getNote(req, res) {
         const id = req.params.id
-        res.send('Notatka została usunięta. ID:' + id)
+        const note = await Note.findOne({
+            _id: id
+        })
+        res.status(200).json(note)
+    }
+    async updateNote(req, res) {
+        const id = req.params.id
+        const title = req.body.title;
+        const body = req.body.body;
+        const note = await  Note.findOne({_id:id})
+        note.title=title
+        note.body=body
+        await note.save()
+        res.status(201).json(note)
+    }
+    async deleteNote(req, res) {
+        const id = req.params.id
+        await Note.deleteOne({_id:id})
+        res.sendStatus(204)
     }
 }
 module.exports = new NoteActions()
